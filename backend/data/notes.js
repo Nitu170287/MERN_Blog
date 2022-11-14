9eae112aa2485514f3812b8da1422b1d55aa0994
Nitu Dubey <nitutiwari01@gmail.com>
2022-11-14 13:57:18 -0500

    Initialized frontend and backend

---
+++ backend/data/notes.js
@@ -0,0 +1,19 @@
+const notes = [
+  {
+    _id: "1",
+    title: "html",
+    content: "Learned html tag ",
+  },
+  {
+    _id: "2",
+    title: "css",
+    content: "Learned css ",
+  },
+  {
+    _id: "3",
+    title: "javascript",
+    content: "Learned javascript function ",
+  },
+];
+
+module.exports = notes;
